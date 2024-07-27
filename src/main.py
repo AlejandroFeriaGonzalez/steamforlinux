63f900f9ec39fdc7c9ab8ecf636afc75f4c24520
AlejandroFeriaGonzalez <aferiag@unal.edu.co>
2024-07-26 20:14:54 -0500

    Update dependencies and switch from requests to httpx for async HTTP requests

--- src/main.py
+++ src/main.py
@@ -1,69 +1,45 @@
-# to run the server, use the following command:
-# fastapi dev .\src\main.py
-
-import json
-import pathlib
-
 from fastapi import FastAPI, Request
-from fastapi.responses import HTMLResponse
+from fastapi.responses import HTMLResponse, RedirectResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 
 from .models import models
-from .services.steamApi import getGameInfo, getOwnedGames, requests
+from .services.steamApi import getGameInfo, getOwnedGames, httpx
+
+import pathlib
 
 SRC = pathlib.Path(__file__).parent
-
-
-STEAM_API_RESPONSE = SRC / "mocks/steamapi_response.json"
-
-try:
-    with open(STEAM_API_RESPONSE) as f:
-        data = json.load(f)
-        steam_api_response = models.SteamAPIResponse(**data)
-except FileNotFoundError:
-    raise FileNotFoundError("File not found")
-
 
 app = FastAPI()
 app.mount("/static", StaticFiles(directory=SRC / "templates"), name="static")
 templates = Jinja2Templates(directory=SRC / "templates/")
 
-# @app.get("/")
-# async def read_root():
-#     # return data['response']['games']
-#     games = steam_api_response.response.games
-#     response = []
-#     for game in games:
-#         data = fetch_operating_system_requirements(game.appid)
-#         response.append(data)
-#     return response
-
+@app.get("/")
+async def root():
+    return RedirectResponse(url="/index")
 
 @app.get("/index", response_class=HTMLResponse)
 async def index(request: Request):
     return templates.TemplateResponse(request, "index.html")
 
-
 @app.get("/get_games/", response_class=HTMLResponse)
 async def get_games(request: Request, steamid: int):
     try:
-        data = getOwnedGames(steamid)
+        data = await getOwnedGames(steamid)
         games: list[models.GameInfo] = []
 
         for game in data.response.games:
-            game_info = getGameInfo(game.appid)
+            game_info = await getGameInfo(game.appid)
             games.append(game_info)
         return templates.TemplateResponse(
             request, "responseList.html", {"games": games}
         )
-    except requests.exceptions.RequestException as e:
+    except httpx.RequestError as e:
         return templates.TemplateResponse(request, "error.html", {"error": str(e)})
 
-
 @app.get("/game/")
-def read_game(appid: int):
+async def read_game(appid: int):
     try:
-        return getGameInfo(appid)
-    except requests.exceptions.RequestException as e:
+        return await getGameInfo(appid)
+    except httpx.RequestError as e:
         return {"error": str(e)}
