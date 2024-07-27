b2b1a58bcab30f6b50dbc0955487c311daa845aa
AlejandroFeriaGonzalez <aferiag@unal.edu.co>
2024-07-27 15:48:11 -0500

    documentation and handling errors

--- src/main.py
+++ src/main.py
@@ -3,7 +3,8 @@
 import pathlib
 
 from fastapi import FastAPI, Request
-from fastapi.responses import HTMLResponse, RedirectResponse
+from fastapi.exceptions import RequestValidationError
+from fastapi.responses import HTMLResponse, JSONResponse, RedirectResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 
@@ -12,7 +13,23 @@ from .services.steamApi import getGameInfo, getOwnedGames, httpx
 
 SRC = pathlib.Path(__file__).parent
 
-app = FastAPI()
+app = FastAPI(
+    title="Steam For Linux",
+    description="An API to retrieve information about Steam games and owned games for a given user.",
+    version="0.1",
+    summary="Steam Game Info API",
+    terms_of_service="http://example.com/terms/",
+    license_info={
+        "name": "Apache 2.0",
+        "url": "https://www.apache.org/licenses/LICENSE-2.0.html",
+    },
+    contact={
+        "name": "Deadpoolio the Amazing",
+        "url": "http://x-force.example.com/contact/",
+        "email": "dp@x-force.example.com",
+    },
+)
+
 app.mount("/static", StaticFiles(directory=SRC / "templates"), name="static")
 templates = Jinja2Templates(directory=SRC / "templates/")
 
@@ -41,6 +58,8 @@ async def get_games(request: Request, steamid: int):
         )
     except httpx.RequestError as e:
         return templates.TemplateResponse(request, "error.html", {"error": str(e)})
+    except httpx.HTTPStatusError as e:
+        return templates.TemplateResponse(request, "error.html", {"error": str(e)})
 
 
 @app.get("/game/")
@@ -49,3 +68,11 @@ async def read_game(appid: int):
         return await getGameInfo(appid)
     except httpx.RequestError as e:
         return {"error": str(e)}
+
+
+@app.exception_handler(RequestValidationError)
+async def validation_exception_handler(request: Request, exc: RequestValidationError):
+    return JSONResponse(
+        status_code=422,
+        content={"detail": exc.errors(), "body": exc.body},
+    )
--- src/services/steamApi.py
+++ src/services/steamApi.py
@@ -10,6 +10,18 @@ KEY = os.getenv("STEAM_API_KEY")
 
 
 async def getOwnedGames(steamid: int) -> models.SteamAPIResponse:
+    """
+    Retrieves the list of owned games for a given Steam ID.
+
+    Args:
+        steamid (int): The Steam ID of the user.
+
+    Returns:
+        models.SteamAPIResponse: An instance of the SteamAPIResponse model class containing the response data.
+
+    Raises:
+        httpx.RequestError: If there is an error while making the HTTP request.
+    """
     url = f"http://api.steampowered.com/IPlayerService/GetOwnedGames/v0001/?key={KEY}&steamid={steamid}&format=json"
 
     try:
@@ -19,9 +31,24 @@ async def getOwnedGames(steamid: int) -> models.SteamAPIResponse:
             return models.SteamAPIResponse(**r.json())
     except httpx.RequestError as e:
         raise e
+    except httpx.HTTPStatusError as e:
+        raise e
 
 
-async def getGameInfo(appid: int):
+async def getGameInfo(appid: int) -> models.GameInfo:
+    """
+    Retrieves game information from the Steam API based on the provided appid.
+
+    Args:
+        appid (int): The unique identifier of the game on Steam.
+
+    Returns:
+        models.GameInfo: An instance of the GameInfo model containing the retrieved game information.
+
+    Raises:
+        httpx.RequestError: If there is an error while making the HTTP request to the Steam API.
+    """
+
     base_url = f"https://store.steampowered.com/api/appdetails?appids={appid}"
 
     try:
